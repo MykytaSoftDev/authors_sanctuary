05ede3f8efe300e029df4558f943e39489e77bae
mykyta <mykyta.naz.prog@gmail.com>
2023-10-06 20:14:47 -0400

    add jwt auth and auth routes

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -12,6 +12,6 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
 EMAIL_PORT = env("EMAIL_PORT")
-DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+DEFAULT_FROM_EMAIL = "support@authors.site"
 DOMAIN = env("DOMAIN")
 SITE_NAME = "Authors Sanctuary"
