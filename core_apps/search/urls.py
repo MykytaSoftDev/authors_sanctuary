1f80683ed1e39daecbbd10bf94cb390418fece14
Mykyta Nazarenko <mykyta.naz.prog@gmail.com>
2023-10-17 16:21:12 -0700

    add elasticsearch for article model

---
+++ core_apps/search/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import ArticleEslasticSearchView
+
+urlpatterns = [
+    path("search/", ArticleEslasticSearchView.as_view({"get": "list"}), name="article_search",)
+]
